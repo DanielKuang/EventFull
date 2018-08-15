62eaba9e2dc24a4c0e4b581652cfee637f951d8d
tectonic8 <dr.harteksab@gmail.com>
2018-08-15 02:58:42 -0400

    Very rudimentary implementation of search bar moving to nav bar

--- js/main.js
+++ js/main.js
@@ -2,8 +2,18 @@ $('#carouselExampleIndicators').carousel();
 $(window).scroll(function() {
     var scrollTop = $(this).scrollTop();
     var elementHeight = $(this).height();
+    var searchField = $('.search-field');
+    var searchBarPos = searchField.offset().top;
     var alpha = Math.min(1-((elementHeight/2 - scrollTop) / (elementHeight/2)), 1);
-    console.log(alpha);
+    console.log("scroll is " + scrollTop + " and bar is at " + searchBarPos);
+    var navBarHeight = $('.navbar').height();
     $('.navbar').css('background-color', 'rgba(10,10,10,' + alpha + ')');
+    if (scrollTop + navBarHeight > searchBarPos) {
+        searchField.css('width', '80%');
+        searchField.css('height', ''+navBarHeight*0.7);
+        searchField.css('top', '0');
 
+        searchField[0].placeholder = '';
+        searchField.appendTo($('.nav-search'));
+    }
 });
