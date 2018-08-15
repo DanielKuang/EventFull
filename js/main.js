d12ed185396f965e1cb78cf59ad69df515db4dbc
tectonic8 <dr.harteksab@gmail.com>
2018-08-15 02:14:14 -0400

    main.js is in, opacity of navbar increases with scroll, navbar fixed position

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 $('#carouselExampleIndicators').carousel();
+$(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    var elementHeight = $(this).height();
+    var alpha = Math.min(1-((elementHeight/2 - scrollTop) / (elementHeight/2)), 1);
+    console.log(alpha);
+    $('.navbar').css('background-color', 'rgba(10,10,10,' + alpha + ')');
 
+});
